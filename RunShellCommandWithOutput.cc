4d42b7d9997d308bd498bc97410b045957bf328c
cullub <gregoryplace@icloud.com>
2020-07-29 10:44:38 -0700

    Create RunShellCommandWithOutput.cc

---
+++ RunShellCommandWithOutput.cc
@@ -0,0 +1,18 @@
+// Run a system (shell) command (linux only, I think), and capture the output of the command.
+
+// https://www.jeremymorgan.com/tutorials/c-programming/how-to-capture-the-output-of-a-linux-command-in-c/
+std::string GetStdoutFromCommand(std::string cmd) {
+  std::string data;
+  FILE * stream;
+  const int max_buffer = 256;
+  char buffer[max_buffer];
+  cmd.append(" 2>&1");
+
+  stream = popen(cmd.c_str(), "r");
+  if (stream) {
+    while (!feof(stream))
+      if (fgets(buffer, max_buffer, stream) != NULL) data.append(buffer);
+        pclose(stream);
+    }
+  return data;
+}
